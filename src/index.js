751ddffb05ff59f66f838674bb82fe3f89fa1696
Oghenemeru G. Avwemoya <skiboss4life@gmail.com>
2022-11-29 13:27:52 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+//Create a react app from scratch.
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const name = "Meru Avwemoya";
+var year = new Date().getFullYear();
+
+ReactDOM.render(
+  <div>
+    <p>
+      Created by <strong>{name}</strong>
+    </p>
+    <p>Copyright {year}</p>
+  </div>,
+  document.getElementById("root")
+);
